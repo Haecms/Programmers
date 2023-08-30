28781e5413ddcb5cf8d2f20d9f7442fa2995d074
Haecms <137016400+Haecms@users.noreply.github.com>
2023-08-31 01:19:59 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.46 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -0,0 +1,11 @@
+using System;
+public class Solution {
+    public string solution(string[] seoul)
+    {
+        int answer = 0;
+
+        answer = Array.FindIndex(seoul, i => i == "Kim");
+
+        return($"김서방은 {answer}에 있다");
+    }
+}
