22532ce5c780d1f37679a6c7d6bf31f2c4b1c408
Haecms <137016400+Haecms@users.noreply.github.com>
2023-08-31 01:38:35 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.19 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers)
+    {
+        int Max = numbers[0]*numbers[1];
+        for(int i=0; i<numbers.Length; i++)
+        {
+            for(int j= i+1; j<numbers.Length; j++)
+            {
+                if(numbers[i]*numbers[j]>= Max)
+                {
+                    Max = numbers[i]*numbers[j];
+                }
+            }
+        }
+        int answer = Max;
+        return answer;
+    }
+}
